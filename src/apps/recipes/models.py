51ca348f924596aa96f4efa3cbbc68adefa622be
aleha2001 <alehak2001@gmail.com>
2023-10-27 12:52:08 +0300

    Add Recipes model

--- src/apps/recipes/models.py
+++ src/apps/recipes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from django.conf import settings
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """
+    Recipe model
+    """
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField()
+    full_text = models.TextField(max_length=5000, blank=True)
+    short_text = models.CharField()
+    ingredient = ""  # TODO: ForeignKey
+    tag = ""  # TODO: ForeignKey
+    category = ""  # TODO: ForeignKey
+    cooking_time = models.PositiveIntegerField(validators=[MaxValueValidator(60 * 24)])
+    views = ""  # TODO: ForeignKey
+    reactions = ""  # TODO: ForeignKey
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.short_text = self.full_text[:100]  # TODO: round by word?
+        super().save(*args, **kwargs)
