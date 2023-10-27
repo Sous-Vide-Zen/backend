2d71ea924cdb3832bb9f903d50514f26966ccc0d
aleha2001 <alehak2001@gmail.com>
2023-10-27 16:37:59 +0300

    Fix Comments

--- src/apps/recipes/models.py
+++ src/apps/recipes/models.py
@@ -8,16 +8,15 @@ class Recipe(models.Model):
     Recipe model
     """
 
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField()
     full_text = models.TextField(max_length=5000, blank=True)
     short_text = models.CharField()
-    ingredient = ""  # TODO: ForeignKey
-    tag = ""  # TODO: ForeignKey
-    category = ""  # TODO: ForeignKey
+    #ingredient = models.ManyToManyField('Ingredient',related_name='recipes')
+    #tag = models.ManyToManyField('Tag',related_name='recipes')
+    #category = models.ManyToManyField('Category',related_name='recipes')
     cooking_time = models.PositiveIntegerField(validators=[MaxValueValidator(60 * 24)])
-    views = ""  # TODO: ForeignKey
-    reactions = ""  # TODO: ForeignKey
+    # views, reactions - foreign keys in Views, Reactions
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
