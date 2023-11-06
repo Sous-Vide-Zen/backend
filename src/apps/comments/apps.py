76c670218575aaadd6deed4071e00d499f282463
aleha2001 <alehak2001@gmail.com>
2023-11-06 23:01:59 +0200

    Comment migrtaion added

--- src/apps/comments/apps.py
+++ src/apps/comments/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CommentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "comments"
+    name = "src.apps.comments"
--- src/apps/comments/models.py
+++ src/apps/comments/models.py
@@ -7,7 +7,11 @@ class Comment(models.Model):
     Comments model
     """
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    recipe = models.ForeignKey("recipes.Recipe", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+    recipe = models.ForeignKey(
+        "recipes.Recipe", on_delete=models.CASCADE, related_name="comments"
+    )
     text = models.TextField(max_length=1000)
     timestamp = models.DateTimeField(auto_now_add=True)
