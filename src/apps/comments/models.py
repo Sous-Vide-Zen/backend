503626ee48f97950e9d4265b93cc087f67c40000
aleha2001 <alehak2001@gmail.com>
2023-11-09 19:35:43 +0200

    Comments fields renamed, parent key added

--- src/apps/comments/models.py
+++ src/apps/comments/models.py
@@ -7,11 +7,25 @@ class Comment(models.Model):
     Comments model
     """
 
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name="comments",
+        null=True,
     )
     recipe = models.ForeignKey(
-        "recipes.Recipe", on_delete=models.CASCADE, related_name="comments"
+        "recipes.Recipe", on_delete=models.SET_NULL, related_name="comments", null=True
     )
     text = models.TextField(max_length=1000)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey(
+        "self", on_delete=models.DO_NOTHING, null=True, blank=True
+    )
+
+    def delete(self, using=None, keep_parents=False):
+        """
+        to copy parent of deleted object
+        """
+        self.parent = self.parent.parent
+        self.save()
+        super(Comment, self).delete(using=using, keep_parents=keep_parents)
