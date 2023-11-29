57f135be8b086051272fbc78cbcfacd3304f7c40
aleha2001 <alehak2001@gmail.com>
2023-11-29 17:36:13 +0200

    Category model added

--- src/apps/recipes/models.py
+++ src/apps/recipes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from taggit.managers import TaggableManager
 
 from src.apps.api.services import shorten_text
+from django.utils.text import slugify
 
 
 class Recipe(models.Model):
@@ -13,14 +14,13 @@ class Recipe(models.Model):
 
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     full_text = models.TextField(max_length=5000, blank=True)
     short_text = models.CharField(max_length=200)
-    # ingredient = models.ManyToManyField('Ingredient',related_name='recipes')
     tag = TaggableManager()
-    # category = models.ManyToManyField('Category',related_name='recipes')
+    category = models.ManyToManyField("Category", related_name="recipes")
     cooking_time = models.PositiveIntegerField(validators=[MaxValueValidator(60 * 24)])
-    # views, reactions - foreign keys in Views, Reactions
+    # views, reactions - foreign keys in Views, Reactions, ingredients - many to many field in IngredientToRecipe model
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -28,4 +28,14 @@ class Recipe(models.Model):
 
     def save(self, *args, **kwargs):
         self.short_text = shorten_text(self.full_text, 100)
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
         super().save(*args, **kwargs)
