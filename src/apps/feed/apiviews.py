d3c14a7f191587561e626e9dc952fa6131f987e1
aleha2001 <alehak2001@gmail.com>
2024-01-12 17:43:50 +0200

    Pagination and sorting added

--- src/apps/feed/apiviews.py
+++ src/apps/feed/apiviews.py
@@ -1,16 +1,24 @@
 from rest_framework import viewsets
 from src.apps.recipes.models import Recipe
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import OrderingFilter
 from .serializers import FeedSerializer
+
+
+class FeedUserPagination(PageNumberPagination):
+    page_size = 5
 
 
 class FeedUserList(viewsets.ViewSet):
     """
-    Listing all created user's posts
+    Listing all created user's posts with pagination and sorting by pub_date
     """
 
     def list(self, request):
+        paginator = FeedUserPagination()
         user = request.user
-        queryset = Recipe.objects.filter(author=user)
-        serializer = FeedSerializer(queryset, many=True)
-        return Response(serializer.data)
+        queryset = Recipe.objects.filter(author=user).order_by("-pub_date")
+        page = paginator.paginate_queryset(queryset, request)
+        serializer = FeedSerializer(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
