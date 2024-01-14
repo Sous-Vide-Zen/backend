6bf1adcf6e56d9cb9f98466fe6342171c9acd74f
aleha2001 <alehak2001@gmail.com>
2024-01-14 18:11:50 +0200

    Feed serializer fixes

--- src/apps/feed/serializers.py
+++ src/apps/feed/serializers.py
@@ -1,20 +1,30 @@
 from src.apps.recipes.models import Recipe
 from rest_framework import serializers
 from taggit.serializers import TagListSerializerField, TaggitSerializer
+from src.apps.reactions.models import Reaction
+
+
+class ReactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reaction
+        fields = ["author", "pub_date", "emoji"]
 
 
 class FeedSerializer(serializers.ModelSerializer):
-    comments_count = serializers.SerializerMethodField("get_comments_count")
-    views_count = serializers.SerializerMethodField("get_view_count")
-    reactions_count = serializers.SerializerMethodField("get_reactions")
+    comments_count = serializers.SerializerMethodField()
+    views_count = serializers.SerializerMethodField()
+    reactions = serializers.SerializerMethodField()
     tag = TagListSerializerField()
     author = serializers.CharField(source="author.username")
+    activity_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Recipe
         fields = [
             "id",
             "title",
+            "slug",
+            "category",
             "short_text",
             "preview_image",
             "author",
@@ -23,15 +33,22 @@ class FeedSerializer(serializers.ModelSerializer):
             "cooking_time",
             "comments_count",
             "views_count",
-            "reactions_count",
+            "reactions",
             "tag",
+            "reactions",
+            "activity_count",
         ]
 
     def get_comments_count(self, obj):
         return obj.comments.count()
 
-    def get_view_count(self, obj):
+    def get_views_count(self, obj):
         return obj.views.count()
 
     def get_reactions(self, obj):
-        return obj.reactions.count()
+        reactions = Reaction.objects.filter(is_deleted=False)
+        serializer = ReactionSerializer(reactions, many=True)
+        return serializer.data
+
+    def get_activity_count(self, obj):
+        return obj.comments.count() + obj.views.count() + obj.reactions.count()
