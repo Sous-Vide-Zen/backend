3976b4c03a892d1c7eaf1bb5ffe16bcfc1a72acb
aleha2001 <alehak2001@gmail.com>
2024-01-21 20:34:20 +0200

    ViewRecipe count test removed

--- src/tests/view/test_view.py
+++ src/tests/view/test_view.py
@@ -6,11 +6,6 @@ from src.apps.view.models import ViewRecipes
 
 @pytest.mark.django_db
 class TestViewRecipesModel:
-    def test_initial_view_count(self, new_user, new_recipe):
-        # Проверка начального значения count при создании новой записи
-        view = ViewRecipes.objects.create(user=new_user.username, recipe=new_recipe)
-        assert view.count == 0
-
     def test_view_recipe_uniqueness(self, new_user, new_recipe):
         # Создание записи о просмотре и попытка создать дублирующую запись
         ViewRecipes.objects.create(user=new_user.username, recipe=new_recipe)
