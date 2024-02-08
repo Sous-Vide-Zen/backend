69f6765f868a247a5a9ebe795860cafa79684288
exites <or.strangelove@gmail.com>
2024-02-08 03:16:27 +0300

    overridden joser methods list and {id}

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,22 +1,38 @@
-from src.apps.users.permissions import IsOwnerOrAdmin
+from rest_framework.exceptions import NotFound
+from src.base.permissions import IsOwnerOrAdminOrReadOnly
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from drf_yasg.utils import swagger_auto_schema
+
+
 from djoser.views import UserViewSet
 
 
 class CustomUserMeViewSet(UserViewSet):
+    @swagger_auto_schema(exclude=["update", "partial_update", "destroy"])
     @action(["get"], detail=False)
     def me(self, request, *args, **kwargs):
-        # Переопределение метода retrieve для получения данных текущего пользователя
         self.get_object = self.get_instance
         return self.retrieve(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        raise NotFound("Endpoint is disabled.")
+
+    def update(self, request, *args, **kwargs):
+        raise NotFound("Endpoint is disabled.")
+
+    def partial_update(self, request, *args, **kwargs):
+        raise NotFound("Endpoint is disabled.")
+
+    def destroy(self, request, *args, **kwargs):
+        raise NotFound("Endpoint is disabled.")
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = [IsOwnerOrAdmin]
+    permission_classes = [IsOwnerOrAdminOrReadOnly]
     lookup_field = "username"
