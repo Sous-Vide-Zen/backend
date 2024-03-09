a3b25c1b848170ed974138d8886da1e29740ed48
Мухамед <S0vetbek0v.muhamed@gmail.com>
2024-03-10 02:07:48 +0600

    translates server responces and fix tests

--- src/tests/base/test_validator.py
+++ src/tests/base/test_validator.py
@@ -1,7 +1,7 @@
 import pytest
 from django.core.exceptions import ValidationError
 
-from src.apps.users.validators import CustomPasswordValidator
+from src.base.validators import CustomPasswordValidator
 
 
 @pytest.mark.parametrize(
