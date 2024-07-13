f97addc3b9aaeb1f7d28d89610d612adbdf50756
chrnmaxim <chrn.maxim@gmail.com>
2024-07-13 15:06:28 +0300

    Change published filed default in Recipe model

--- src/apps/recipes/models.py
+++ src/apps/recipes/models.py
@@ -68,7 +68,7 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     reactions = GenericRelation(Reaction, related_query_name="recipe_reactions")
     is_repost = models.BooleanField(default=False)
-    published = models.BooleanField(default=True)
+    published = models.BooleanField(default=False)
 
     class Meta:
         index_together = ["title", "slug"]
