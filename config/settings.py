a73c6db25e975e9a046dd85e29e8a4fe4d90e70f
max <mcnic@users.noreply.github.com>
2024-09-04 10:58:35 -0700

    fixL settings.py format

--- config/settings.py
+++ config/settings.py
@@ -139,18 +139,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sous-vide-zen-db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "sous-vide-zen-db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     },
     "sqlite": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
 }
 
 # Password validation
