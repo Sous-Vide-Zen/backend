bc028dd84db94faab54cf2fbfa12245b107217ce
chrnmaxim <chrn.maxim@gmail.com>
2024-11-12 13:08:55 +0300

    Add recipes search in `FeedUserList`

--- src/apps/feed/views.py
+++ src/apps/feed/views.py
@@ -22,9 +22,14 @@ class FeedUserList(mixins.ListModelMixin, viewsets.GenericViewSet):
 
     pagination_class = FeedPagination
     serializer_class = FeedSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
     ordering_fields = ["activity_count"]
     ordering = ["-pub_date"]
+    search_fields = ["title", "full_text", "short_text"]
     filterset_class = FeedFilter
 
     def get_queryset(self):
