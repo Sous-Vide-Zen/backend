4a19f567c5679494b5071504d08af1e377caca6e
alelsandr <sanya10081999@gmail.com>
2024-12-24 10:53:50 +0700

    fix: add doc to CustomOIDCAuthenticationBackend and some refactoring code

--- src/apps/users/authentication.py
+++ src/apps/users/authentication.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework.exceptions import AuthenticationFailed
 from django.db import transaction
-from django.utils.module_loading import import_string
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
 
@@ -9,16 +8,16 @@ User = get_user_model()
 
 
 class CustomOIDCAuthenticationBackend(OIDCAuthenticationBackend):
-    def get_username(self, claims):
-        username_algo = self.get_settings("OIDC_USERNAME_ALGO", None)
-        if username_algo:
-            if isinstance(username_algo, str):
-                username_algo = import_string(username_algo)
-            return username_algo(claims.get("email"))
-        if not claims.get("email"):
-            raise AuthenticationFailed("Email is required")
+    """
+    Аутентификация, через OIDC + создание и обновление пользователя в БД.
+    В claims получаем данные пользователя из keycloak token и сверяем с данными в БД
+    """
 
-        return claims.get("email")
+    def get_username(self, claims):
+        email = claims.get("email")
+        if not email:
+            raise AuthenticationFailed("Email is required")
+        return email
 
     def filter_users_by_claims(self, claims):
         email = self.get_username(claims)
@@ -34,13 +33,9 @@ class CustomOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         return user
 
     def update_user(self, user, claims):
-        changed = False
         email = self.get_username(claims)
         if user.email != email:
             user.email = email
-            changed = True
-        if changed:
-            user.save()
-            user.refresh_from_db()
+            user.save(update_fields=["email"])
 
         return user
