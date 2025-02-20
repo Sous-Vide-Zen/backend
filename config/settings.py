c1e4365eea30e43a6b94ed1d1084c339105dc9c2
vbamnup <vbamnup@gmail.com>
2025-02-20 13:03:53 +0300

    Fix static url

--- config/settings.py
+++ config/settings.py
@@ -186,7 +186,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = "src/static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "src/static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "src/media"
@@ -264,8 +265,3 @@ TIME_FROM_VIEW_RECIPE = 20
 # Regex for custom user
 
 REGEX = r"^[a-zA-Zа-яА-Я\s\-\‘\u00C0-\u017F]+$"
-
-# try:
-#     from .local_settings import *
-# except ImportError:
-#     pass
