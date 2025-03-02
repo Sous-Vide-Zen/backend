ffdafbf4f83084faac6a733f0c2e7acea4ef2e0d
vbamnup <vbamnup@gmail.com>
2025-03-02 16:46:04 +0300

    Fix user activation

--- src/apps/users/routes/django_urls.py
+++ src/apps/users/routes/django_urls.py
@@ -45,13 +45,13 @@ router.register("user", CustomUserViewSet, basename="user")
 
 
 urlpatterns = [
+    path("", include(router_for_djoser.urls)),
+    path("", include(router.urls)),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
     path(
         "auth/users/activate/<uid>/<token>/",
         UserActivationViewSet.as_view({"get": "activation"}),
         name="activation",
     ),
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.jwt")),
-    path("", include(router_for_djoser.urls)),
-    path("", include(router.urls)),
 ]
